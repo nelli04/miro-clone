50287be9b5b2fd8403efe8577a584f5382b0c754
Nelly_Ivanova <nelli_ivanova04@mail.ru>
2025-01-16 21:20:54 +0300

    feat(lib): add get contrasting text color utils

---
+++ lib/get-contrasting-text-color.ts
@@ -0,0 +1,7 @@
+import { Color } from "@/types/canvas";
+
+export function getContrastingTextColor(color: Color) {
+  const luminance = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
+
+  return luminance > 182 ? "black" : "white";
+}
