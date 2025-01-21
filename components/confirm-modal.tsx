e1bbb1e07bf44a5b5b141283bce6c5808bc255a7
Nelly_Ivanova <nelli_ivanova04@mail.ru>
2025-01-21 14:12:23 +0300

    feat(component): add confirm modal component

---
+++ components/confirm-modal.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+type ConfirmModalProps = {
+  children?: ReactNode;
+  onConfirm: () => void;
+  disabled?: boolean;
+  header: string;
+  description?: string;
+};
+
+export const ConfirmModal = ({
+  onConfirm,
+  header,
+  disabled,
+  description,
+  children,
+}: ConfirmModalProps) => {
+  const onClick = () => {
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{header}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction disabled={disabled} onClick={onClick}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
