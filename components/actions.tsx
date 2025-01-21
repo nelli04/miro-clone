45df7d2ff9db4f00047a4efca1be29048f3c6c78
Nelly_Ivanova <nelli_ivanova04@mail.ru>
2025-01-21 14:11:54 +0300

    feat(component): add actions component

---
+++ components/actions.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { ReactNode } from "react";
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Link2, Pencil, Trash2 } from "lucide-react";
+import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { ConfirmModal } from "@/components/confirm-modal";
+import { Button } from "@/components/ui/button";
+import { useRenameModal } from "@/store/use-rename-modal";
+
+type ActionsProps = {
+  children: ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+};
+
+export const Actions = ({
+  side,
+  sideOffset,
+  id,
+  title,
+  children,
+}: ActionsProps) => {
+  const { onOpen } = useRenameModal();
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/boards/${id}`)
+      .then(() => {
+        toast.success("Link copied!");
+      })
+      .catch(() => toast.error("Failed to copy link"));
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted!"))
+      .catch(() => toast.error("Failed to delete board!"));
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        sideOffset={sideOffset}
+        side={side}
+        className="w-60"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="p-3 cursor-pointer"
+          onClick={() => onOpen(id, title)}
+        >
+          <Pencil className="h-4 w-4 mr-2" />
+          Rename
+        </DropdownMenuItem>
+        <ConfirmModal
+          header="Delete board?"
+          onConfirm={onDelete}
+          description="This will delete the board and all its contents."
+          disabled={pending}
+        >
+          <Button
+            variant="ghost"
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+          >
+            <Trash2 className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
