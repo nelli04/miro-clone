88ab590178c1b722d7953b16ccf5c7d9a0fa5ef5
Nelly_Ivanova <nelli_ivanova04@mail.ru>
2025-01-22 20:18:51 +0300

    feat(lib): add pointer event to canvas point util

---
+++ lib/pointer-event-to-canvas-point.ts
@@ -0,0 +1,9 @@
+import { PointerEvent } from "react";
+import { Camera } from "@/types/canvas";
+
+export function pointerEventToCanvasPoint(e: PointerEvent, camera: Camera) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+}
